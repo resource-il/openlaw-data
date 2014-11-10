26a7692f50fd1852350de0f68617548f8c1eea5b
Yehuda Deutsch <yeh@uda.co.il>
2014-11-10 22:17:22 +0200

    Remove unused methods in Controller.php

--- src/Openlaw/Controller.php
+++ src/Openlaw/Controller.php
@@ -14,45 +14,6 @@ class Controller
         $this->app = $app;
     }
 
-    protected function response($data = '', $headers = [], $code = Response::HTTP_OK)
-    {
-        return new Response($data, $code, $headers);
-    }
-
-    protected function json($data = [], $error = [], $usage = [])
-    {
-        $response = [
-          'content' => $data,
-          'usage' => $usage,
-          'error' => $error,
-          'license' => 'CC-BY',
-          'copyright' => [
-            'year' => '2014',
-            'name' => 'The Public Knowledge Workshop',
-            'url' => 'http://www.hasadna.org.il/',
-          ],
-        ];
-
-        $jsonEncodeOptions = 0;
-        if (!empty($error)) {
-            $jsonEncodeOptions += JSON_FORCE_OBJECT;
-        }
-
-        $json = json_encode($response, $jsonEncodeOptions);
-
-        if (isset($_GET['callback'])) {
-            $json = $_GET['callback'] . '(' . $json . ');';
-        }
-
-        return $this->response($json, ['Content-Type' => 'application/json']);
-    }
-
-    protected function error($data = [])
-    {
-
-        return $this->json([], $data);
-    }
-
     public function errorHandler(\Exception $e, $code)
     {
         return $this->app->json(
